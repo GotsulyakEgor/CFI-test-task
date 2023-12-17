4ddbab57dc3b401653cd63a0655a531af29388b5
misha <suxx_2000@hotmail.com>
2023-12-17 22:21:58 +0100

    feedback

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import {CoursesListComponent} from "./components/courses-list/courses-list.component";
-import {CourseInfoComponent} from "./components/course-info/course-info.component";
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CourseInfoComponent } from './components/course-info/course-info.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/courses', pathMatch: 'full' },
-    { path: 'courses', component: CoursesListComponent },
-    { path: 'courses/:id', component: CourseInfoComponent },
-
+  { path: '', redirectTo: '/courses', pathMatch: 'full' },
+  { path: 'courses', component: CoursesListComponent },
+  { path: 'courses/:id', component: CourseInfoComponent },
 ];
--- frontend/src/app/services/courses.service.ts
+++ frontend/src/app/services/courses.service.ts
@@ -1,26 +1,20 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Course } from '../interfaces/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
+  private http = inject(HttpClient);
   private baseUrl = 'http://localhost:3000';
-  private coursesSubject = new BehaviorSubject<any[]>([]);
-  courses$ = this.coursesSubject.asObservable();
 
-  constructor(private http: HttpClient) {
-    this.loadCourses();
+  getCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`${this.baseUrl}/courses/${courseId}`);
   }
 
-  private loadCourses() {
-    this.http.get<any[]>(`${this.baseUrl}/courses`).subscribe(courses => {
-      this.coursesSubject.next(courses);
-    });
-  }
-
-  getCourseById(courseId: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/courses/${courseId}`);
+  loadCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.baseUrl}/courses`);
   }
 }
